ef01e482b6ad9ecd75f1584cd6e44ff4fa41866e
Andreu Pomar <apomar@cifpfbmoll.eu>
2019-10-30 18:10:42 +0100

    //

---
+++ Ejercicio 7/elimina espacios.py
@@ -0,0 +1,12 @@
+#Andreu Pomar Cabot
+#Ejercicio 7.8 - Eliminador de espacios
+
+def terminator():
+    final = frase.replace(" ","")
+    return final
+
+frase = str(input("Introduce una frase: "))
+
+print (terminator())
+
+input()
